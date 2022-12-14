0fc1b3fd4f2300fcfe9c8bb825f6452a070b0db3
Lukas Oswald <l.v.oswald97@gmail.com>
2022-12-14 17:00:57 +0100

    Tried to have the value of the test key actually become that value

--- src/site/scripts/generateKey.js
+++ src/site/scripts/generateKey.js
@@ -2,4 +2,5 @@ function generateKey() {
     let x = Math.floor ((Math.random() * 10000)+1);
     var s = document.getElementById('test'); //testing the key generator
     s.value = s.value.toString()+ x.toString();
+    document.getElementById('test').innerHTML = localStorage.getItem("s");
 }
