eeb2e39aac5dd13136677a9de95fd2c59dbc30f5
Paulus Timur <paulus.timur@student.htw-berlin.de>
2022-12-14 14:14:06 +0100

    key generator update

--- src/site/scripts/generateKey.js
+++ src/site/scripts/generateKey.js
@@ -1,5 +1,12 @@
 function generateKey() {
     let x = Math.floor ((Math.random() * 10000)+1);
-    var s = document.getElementById('test'); //testing the key generator
-    s.value = s.value.toString()+ x.toString();
+    let y = x.valueOf();
+    if (y < 10 ) {
+        y = "000"+x.toString();}
+    if (y < 100 &&x >= 10) {
+        y = "00"+x.toString();}
+    if (y < 1000 &&x >= 100) {
+        y = "0"+x.toString();}
+    const s = document.getElementById('test'); //testing the key generator
+    s.value = s.value.toString()+ y;
 }
