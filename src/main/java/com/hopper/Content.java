7b0bf1c74068ac6175a77fedbec0adbdaa35b6bc
Lukas Oswald <l.v.oswald97@gmail.com>
2022-12-29 15:29:30 +0100

    Test of Keycreate, new Content Parts, and other functions.

--- src/main/java/com/hopper/Content.java
+++ src/main/java/com/hopper/Content.java
@@ -5,11 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Content {
+public class Content implements Key{
         @Id
         @GeneratedValue
         private Long id;
         private String userInput;
+    private String KeyOffset;
+    private String Hopperkey;
 
     public void setId(Long id) {
         this.id = id;
@@ -26,4 +28,29 @@ public class Content {
     public void setUserInput(String userInput) {
         this.userInput = userInput;
     }
+
+    public String KeyCreate() {
+        int value = (int)Math.round((Math.random() * 10000)+1);
+        String Stringvalue = Integer.toString(value);
+        String FinalKey = this.KeyOffset + Stringvalue;
+        return FinalKey;
+    }
+
+    public String GetHopperkey(){
+        this.Hopperkey = KeyCreate();
+        return this.Hopperkey;
+    }
+
+    public void setHopperkey(String Offset){
+        this.KeyOffset = Offset;
+        String NewKey = this.KeyCreate();
+        this.Hopperkey = NewKey;
+    }
+
+    public String getKeyOffset(){
+        return KeyOffset;
+    }
+     public void setKeyOffset(String KeyOffset){
+        this.KeyOffset = KeyOffset;
+     }
 }
