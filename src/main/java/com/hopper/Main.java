99ad84095dfe20c3af823c50ba5cf08183ecda26
Walter Weiss <s058099@htw-berlin.de>
2023-01-09 04:48:50 +0100

    Draft: Tested a method to wipe the database by adding a corresponding button

--- src/main/java/com/hopper/Main.java
+++ src/main/java/com/hopper/Main.java
@@ -11,6 +11,9 @@ import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Route("")
 public class Main extends VerticalLayout{
 
@@ -19,32 +22,51 @@ public class Main extends VerticalLayout{
 
     private TextField KeyOffset = new TextField("Enter your Key Offset here");
 
+    private TextField KeyInput = new TextField("Enter your given key here");
+
     private Paragraph HopperKey = new Paragraph("Welcome to Hopper!");
 
+    //outputGrid is where the user's uploaded text is returned
     private Grid<Content> outputGrid = new Grid<>(Content.class);
     private Binder<Content> binder = new Binder<>(Content.class);
 
-
     public Main(ContentDatabase contentDatabase){
         this.contentDatabase = contentDatabase;
+
         binder.forField(userInput).bind(Content::getUserInput,Content::setUserInput);
         binder.forField(KeyOffset).bind(Content::getKeyOffset,Content::setKeyOffset);
+
         var retrieveButton = new Button("Retrieve");
         retrieveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
         retrieveButton.addClickListener(e -> {
             retrieveOutput();
         });
+
+        var wipeButton = new Button("Wipe");
+        retrieveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        wipeButton.addClickListener(e -> {
+            wipeContentDatabase();
+        });
+
         outputGrid.setColumns("id","userInput");
-        add(submitUserContent(), outputGrid, retrieveButton);
+        add(submitUserContent(), outputGrid, retrieveButton, wipeButton);
+
+
     }
+
+    /**
+     *
+     * @return
+     */
     public Component submitUserContent(){
 
         var layout = new VerticalLayout();
         var uploadButton = new Button("Upload");
         uploadButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        layout.add(userInput, KeyOffset, uploadButton, HopperKey);
+        layout.add(userInput, KeyOffset, uploadButton, KeyInput, HopperKey);
 
         binder.bindInstanceFields(this);
 
@@ -71,5 +93,21 @@ public class Main extends VerticalLayout{
     private void retrieveOutput(){
 
         outputGrid.setItems(contentDatabase.findAll());
+    }
+
+    private void wipeContentDatabase(){
+        TimerTask timedWipe = new TimerTask() {
+            @Override
+            public void run() {
+                contentDatabase.deleteAll();
+            }
+        };
+
+        Timer timer = new Timer("Timer");
+
+        long delay = 10000;
+        //delay = time in milliseconds, default is 10000 ms/ 10s
+
+        timer.schedule(timedWipe,delay);
     }
 }
