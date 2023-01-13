979083c05d529d299d3191c3e66f999d5312b523
Walter Weiss <s058099@htw-berlin.de>
2023-01-13 05:51:28 +0100

    Draft: fundamentals finished, added JUnit test

--- src/main/java/com/hopper/HopperApplication.java
+++ src/main/java/com/hopper/HopperApplication.java
@@ -3,13 +3,12 @@ package com.hopper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Program will persistently act as a server as long as the computer is running
+ */
 @SpringBootApplication
 public class HopperApplication {
 
-	/**
-	 * Program will run persistently as a server as long as the computer is up
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		SpringApplication.run(HopperApplication.class, args);
 	}
